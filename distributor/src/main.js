710bc31a6b7e70273423cf98f3b02ffa57984dd2
thativam <thativancapeli@outlook.com>
2025-06-10 21:58:45 -0300

    fix: generating mock server to test local functions.

--- distributor/src/main.js
+++ distributor/src/main.js
@@ -80,8 +80,10 @@ async function process() {
 
         const ASTs = [];
         const otherFiles = [];
+        const ASTs_copy = [];
         console.log(`======= Processing server ${s.id} ========`);
         parseCode(ASTs, otherFiles, inputDir);
+        parseCode(ASTs_copy, otherFiles, inputDir);
         serverStructures.push({
             serverInfo: s,
             asts: ASTs,
@@ -89,7 +91,7 @@ async function process() {
         });
         serverStructures.push({
             serverInfo: s_copy,
-            asts: ASTs,
+            asts: ASTs_copy,
             otherFiles: otherFiles
         });
     }
@@ -157,10 +159,6 @@ function parseCode(asts, otherFiles, inputDir) {
         } else if (itemPath.slice(-2) === "js") {
             // if item is an input file, let's parse it
             // Let's parse the file
-            if(relativePath.includes("test") && !config.isTestServer()) {
-                config.setTestServer(true);
-                console.log(`Skipping test file ${relativePath}`);
-            }
             const input = fs.readFileSync(itemPath, { encoding: "utf8" });
 
             // const ast = babelParser.parse(input, {
@@ -229,9 +227,12 @@ function generateStartCode(serverStructures, allExposedFunctions) {
     for (const { serverInfo, asts } of serverStructures) {
         console.log(`Generating start code for server ${serverInfo.id}`);
         const allExposedFunctionsInServer = allExposedFunctions.filter(
-            (rf) => rf.serverInfo.id === serverInfo.id
+            (rf) => rf.serverInfo.id === serverInfo.id.replace("-test-server", "")
         );
-        const newCode = startServerTemplate(
+        const newCode = serverInfo.id.includes("test-server") ?  startTestServerTemplate(
+            serverInfo,
+            allExposedFunctionsInServer
+        ) : startServerTemplate(
             serverInfo,
             allExposedFunctionsInServer
         );
